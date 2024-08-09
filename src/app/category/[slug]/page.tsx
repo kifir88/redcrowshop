4ba5b3ae690e67c8fded6a98d34c94a9f33e2a13
AsgardPavlov <pasgard9@gmail.com>
2024-08-09 03:12:00 +0200

    extract filter

--- src/app/category/[slug]/page.tsx
+++ src/app/category/[slug]/page.tsx
@@ -1,59 +1,9 @@
-import {
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-} from '@headlessui/react'
-import { FunnelIcon, MinusIcon, PlusIcon } from '@heroicons/react/20/solid'
 import CategoryListSortMenu from "@/components/pages/category/category-list-sort-menu";
 import {fetchProductCategories, fetchProducts} from "@/libs/woocommerce-rest-api";
 import ProductCard from "@/components/pages/category/product-card";
 import Breadcrumb from "@/components/breadcrumb";
 import {getCategoryHierarchyBySlug} from "@/libs/helper-functions";
-
-const subCategories = [
-  { name: 'Totes', href: '#' },
-  { name: 'Backpacks', href: '#' },
-  { name: 'Travel Bags', href: '#' },
-  { name: 'Hip Bags', href: '#' },
-  { name: 'Laptop Sleeves', href: '#' },
-]
-const filters = [
-  {
-    id: 'color',
-    name: 'Color',
-    options: [
-      { value: 'white', label: 'White', checked: false },
-      { value: 'beige', label: 'Beige', checked: false },
-      { value: 'blue', label: 'Blue', checked: true },
-      { value: 'brown', label: 'Brown', checked: false },
-      { value: 'green', label: 'Green', checked: false },
-      { value: 'purple', label: 'Purple', checked: false },
-    ],
-  },
-  {
-    id: 'category',
-    name: 'Category',
-    options: [
-      { value: 'new-arrivals', label: 'New Arrivals', checked: false },
-      { value: 'sale', label: 'Sale', checked: false },
-      { value: 'travel', label: 'Travel', checked: true },
-      { value: 'organization', label: 'Organization', checked: false },
-      { value: 'accessories', label: 'Accessories', checked: false },
-    ],
-  },
-  {
-    id: 'size',
-    name: 'Size',
-    options: [
-      { value: '2l', label: '2L', checked: false },
-      { value: '6l', label: '6L', checked: false },
-      { value: '12l', label: '12L', checked: false },
-      { value: '18l', label: '18L', checked: false },
-      { value: '20l', label: '20L', checked: false },
-      { value: '40l', label: '40L', checked: true },
-    ],
-  },
-]
+import Filters from "@/components/pages/category/filters";
 
 export default async function CategorySlag({
   params: { slug },
@@ -89,14 +39,14 @@ export default async function CategorySlag({
           {/*  <span className="sr-only">View grid</span>*/}
           {/*  <Squares2X2Icon aria-hidden="true" className="h-5 w-5" />*/}
           {/*</button>*/}
-          <button
-            type="button"
-            // onClick={() => setMobileFiltersOpen(true)}
-            className="-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden"
-          >
-            <span className="sr-only">Filters</span>
-            <FunnelIcon aria-hidden="true" className="h-5 w-5" />
-          </button>
+          {/*<button*/}
+          {/*  type="button"*/}
+          {/*  // onClick={() => setMobileFiltersOpen(true)}*/}
+          {/*  className="-m-2 ml-4 p-2 text-gray-400 hover:text-gray-500 sm:ml-6 lg:hidden"*/}
+          {/*>*/}
+          {/*  <span className="sr-only">Filters</span>*/}
+          {/*  <FunnelIcon aria-hidden="true" className="h-5 w-5" />*/}
+          {/*</button>*/}
         </div>
       </div>
 
@@ -107,49 +57,7 @@ export default async function CategorySlag({
 
         <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
           {/* Filters */}
-          <form className="hidden lg:block">
-            <h3 className="sr-only">Categories</h3>
-            <ul role="list" className="space-y-4 border-b border-gray-200 pb-6 text-sm font-medium text-gray-900">
-              {subCategories.map((category) => (
-                <li key={category.name}>
-                  <a href={category.href}>{category.name}</a>
-                </li>
-              ))}
-            </ul>
-
-            {filters.map((section) => (
-              <Disclosure key={section.id} as="div" className="border-b border-gray-200 py-6">
-                <h3 className="-my-3 flow-root">
-                  <DisclosureButton className="group flex w-full items-center justify-between bg-white py-3 text-sm text-gray-400 hover:text-gray-500">
-                    <span className="font-medium text-gray-900">{section.name}</span>
-                    <span className="ml-6 flex items-center">
-                          <PlusIcon aria-hidden="true" className="h-5 w-5 group-data-[open]:hidden" />
-                          <MinusIcon aria-hidden="true" className="h-5 w-5 [.group:not([data-open])_&]:hidden" />
-                        </span>
-                  </DisclosureButton>
-                </h3>
-                <DisclosurePanel className="pt-6">
-                  <div className="space-y-4">
-                    {section.options.map((option, optionIdx) => (
-                      <div key={option.value} className="flex items-center">
-                        <input
-                          defaultValue={option.value}
-                          defaultChecked={option.checked}
-                          id={`filter-${section.id}-${optionIdx}`}
-                          name={`${section.id}[]`}
-                          type="checkbox"
-                          className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                        />
-                        <label htmlFor={`filter-${section.id}-${optionIdx}`} className="ml-3 text-sm text-gray-600">
-                          {option.label}
-                        </label>
-                      </div>
-                    ))}
-                  </div>
-                </DisclosurePanel>
-              </Disclosure>
-            ))}
-          </form>
+          <Filters />
 
           {/* Product grid */}
           <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:col-span-3 lg:gap-x-8">
