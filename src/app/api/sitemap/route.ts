7a8b0e5b3431288768d3b06efc45fa59208ca226
AsgardPavlov <pasgard9@gmail.com>
2024-09-06 11:44:45 +0200

    add one level attributes sitemap

--- src/app/api/sitemap/route.ts
+++ src/app/api/sitemap/route.ts
@@ -1,32 +1,61 @@
 import { NextResponse } from 'next/server';
 import { SitemapStream, streamToPromise } from 'sitemap';
 import { createGzip } from 'zlib';
-import {fetchProductCategories} from "@/libs/woocommerce-rest-api";
+import {
+  fetchCustomProductAttributes,
+  fetchProductCategories
+} from "@/libs/woocommerce-rest-api";
 import {fetchFooterPages} from "@/libs/strapi-rest-api";
+import {ProductCategory} from "@/types/woo-commerce/product-category";
+
+// Refactored function to process categories and attributes for the sitemap
+const processCategoriesForSitemap = async (categoriesData: ProductCategory[], sitemap: SitemapStream) => {
+  // Use a for...of loop to handle async calls inside the loop
+  for (const category of categoriesData) {
+    // Write the category URL to the sitemap
+    sitemap.write({ url: `/category/${category.slug}`, changefreq: 'weekly', priority: 0.8 });
+
+    // Fetch product attributes for the current category
+    const productAttributes = await fetchCustomProductAttributes({
+      category_name: category.slug,
+    });
+
+    // Process the product attributes
+    productAttributes.data.forEach((pa) => {
+      const formattedOption = pa.options.map(o => o.slug).join("-i-");
+
+      // Construct and write the attribute combination URL
+      const combinationUrl = `/category/${category.slug}?${pa.slug.replace("pa_", "")}=${formattedOption}`;
+
+      sitemap.write({ url: combinationUrl, changefreq: 'weekly', priority: 0.7 });
+    });
+  }
+};
 
 export async function GET() {
   try {
     // Fetch the product categories
-    const categoriesData = await fetchProductCategories();
-    const strapiFooterPagesData = await fetchFooterPages()
+    const categoriesData = await fetchProductCategories({
+      exclude: [320],
+    });
+    const strapiFooterPagesData = await fetchFooterPages();
+    // const attributesData = await fetchProductAttributes();
 
     // Set up sitemap stream and gzip it
     const sitemap = new SitemapStream({ hostname: `https://www.redcrow.kz` });
     const pipeline = sitemap.pipe(createGzip());
 
-    sitemap.write({ url: `/`, changefreq: 'weekly', priority: 0.8 });
+    sitemap.write({ url: `/`, changefreq: 'weekly', priority: 0.9 });
     sitemap.write({ url: `/shop`, changefreq: 'weekly', priority: 0.8 });
     sitemap.write({ url: `/cart`, changefreq: 'weekly', priority: 0.8 });
 
     // Add each strapi page to the sitemap
     strapiFooterPagesData.data.data.forEach((sfp) => {
-      sitemap.write({ url: `/info/${sfp.attributes.slug}`, changefreq: 'weekly', priority: 0.8 });
+      sitemap.write({ url: `/info/${sfp.attributes.slug}`, changefreq: 'weekly', priority: 0.7 });
     });
 
-    // Add each category to the sitemap
-    categoriesData.data.forEach((category) => {
-      sitemap.write({ url: `/category/${category.slug}`, changefreq: 'weekly', priority: 0.8 });
-    });
+    // Process the categories and product attributes for the sitemap
+    await processCategoriesForSitemap(categoriesData.data, sitemap);
 
     sitemap.end();
 
