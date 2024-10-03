4331bf08f1c7efd88d1fd35dacd608e71458edaa
AsgardPavlov <pasgard9@gmail.com>
2024-10-03 13:13:22 +0200

    fix svgs

--- src/components/pages/cart/cart-list-item.tsx
+++ src/components/pages/cart/cart-list-item.tsx
@@ -10,14 +10,14 @@ import Image from "next/image";
 const MinusSvg = (
   <svg className="h-2.5 w-2.5 text-gray-900" aria-hidden="true"
        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
-    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
           d="M1 1h16"/>
   </svg>
 )
 const PlusSvg = (
   <svg className="h-2.5 w-2.5 text-gray-900" aria-hidden="true"
        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
           d="M9 1v16M1 9h16"/>
   </svg>
 )
@@ -112,7 +112,7 @@ export default function CartListItem({
             >
               <svg className="me-1.5 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                    width="24" height="24" fill="none" viewBox="0 0 24 24">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
                       d="M6 18 17.94 6M18 18 6.06 6" />
               </svg>
               Удалить
