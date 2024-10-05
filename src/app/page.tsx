5d2653d98ea56fc109ca6f1009e42c0bc4a49e27
AsgardPavlov <pasgard9@gmail.com>
2024-10-05 13:09:04 +0200

    optimize home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
 import {fetchCurrencyRates, fetchProducts} from "@/libs/woocommerce-rest-api";
-import {fetchHeroImage, fetchSlider} from "@/libs/strapi-rest-api";
+import {fetchSlider} from "@/libs/strapi-rest-api";
 import PopularProducts from "@/components/pages/home/popular-products";
 import HeroCarousel from "@/components/pages/home/hero-carousel";
 
 export default async function Home() {
-  const currencyRatesData = await fetchCurrencyRates();
-  const popularProductsData = await fetchProducts({
-    orderby: "popularity",
-    order: "desc",
-    per_page: 6,
-  });
-  const starpiHeroImageData = await fetchHeroImage();
-  const starpiSliderData = await fetchSlider();
+  const [
+    currencyRatesData,
+    popularProductsData,
+    starpiSliderData,
+  ] = await Promise.all([
+    fetchCurrencyRates(),
+    fetchProducts({
+      orderby: "popularity",
+      order: "desc",
+      per_page: 6,
+    }),
+    fetchSlider()
+  ])
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between pb-24">
