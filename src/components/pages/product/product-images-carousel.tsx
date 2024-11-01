071712287c45806c46a3abf8760b850edc1a717b
AsgardPavlov <pasgard9@gmail.com>
2024-11-01 21:47:33 +0100

    fix product images carousel

--- src/components/pages/product/product-images-carousel.tsx
+++ src/components/pages/product/product-images-carousel.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import {useState } from "react";
+import { useMemo, useState} from "react";
 import { Image as ProductImage, Product } from "@/types/woo-commerce/product";
 import Image from "next/image";
 import { Carousel } from "flowbite-react";
@@ -12,39 +12,36 @@ export default function ProductImagesCarousel({
   images: Product["images"];
   productImage: ProductImage | null;
 }) {
-  const imageSrc = productImage?.src || "/category/product-image-placeholder.png";
-  const imageAlt = productImage?.alt || "placeholder";
 
-  const imagesArray = images;
+  const [activeSlider, setActiveSlider] = useState(0)
+
+  const imagesArray = useMemo(() => {
+    const updatedImages = [...images];
+    if (!!productImage) {
+      updatedImages.splice(activeSlider, 0, productImage);
+    }
+    return updatedImages;
+  }, [productImage]);
 
   return (
     <div className="mt-8 lg:col-span-7 lg:col-start-1 lg:row-span-3 lg:row-start-1 lg:mt-0">
       <div className="grid grid-cols-1 lg:gap-8">
-        {!!productImage ? (
-          <Image
-            className="aspect-3/4 lg:col-span-2"
-            key={imageSrc}
-            src={imageSrc}
-            alt={imageAlt}
-            height={1509}
-            width={696}
-          />
-        ) : (
-          <Carousel
-            className="aspect-3/4 lg:col-span-2"
-            slide={false}
-          >
-            {imagesArray.map((i) => (
-              <Image
-                key={i.src}
-                src={i.src}
-                alt={i.alt}
-                height={1509}
-                width={696}
-              />
-            ))}
-          </Carousel>
-        )}
+        <Carousel
+          className="aspect-3/4 lg:col-span-2"
+          slide={false}
+          defaultValue={activeSlider}
+          onSlideChange={setActiveSlider}
+        >
+          {imagesArray.map((i) => (
+            <Image
+              key={i?.src || "/category/product-image-placeholder.png"}
+              src={i?.src || "/category/product-image-placeholder.png"}
+              alt={i?.alt || "placeholder"}
+              height={1509}
+              width={696}
+            />
+          ))}
+        </Carousel>
       </div>
     </div>
   );
