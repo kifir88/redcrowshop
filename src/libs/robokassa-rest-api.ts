437b76d8c7a152eb3628e5bfe3ca8954161ff14f
vlad-che <vlad1010che@gmail.com>
2024-11-08 14:36:31 +0500

    robokassa prod mode

--- src/libs/robokassa-rest-api.ts
+++ src/libs/robokassa-rest-api.ts
@@ -6,11 +6,11 @@ const robokassaApiInstance = new RoboGate({
   // REQUIRED OPTIONS:
   merchantLogin: 'redcrowshop',
   hashingAlgorithm: 'md5',
-  password1: 'thBxjea111OWTIu7Y7kp',
-  password2: 'ZO5DfOO2tTYF4cB78IUV',
+  password1: 'HYeX5Kh8yC1HdP63sfZN',
+  password2: 'T0Wi5pt4M51wkfWLMnUe',
 
   // OPTIONAL CONFIGURATION
-  testMode: true, // Whether to use test mode globally
+  testMode: false, // Whether to use test mode globally
   testPassword1: "MJ1hL68FHYwLJO8CrJ5D",
   testPassword2: "ejjTYhz7Dm4IDj99Pj5H",
   resultUrlRequestMethod: "POST", // !ONLY ACCEPTED METHOD FOR NOW
