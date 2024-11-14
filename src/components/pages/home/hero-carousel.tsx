a2e12840db608173ecb452a45f7b044fdeb21fdc
vlad-che <vlad1010che@gmail.com>
2024-11-14 12:04:01 +0500

    remove arrows

--- src/components/pages/home/hero-carousel.tsx
+++ src/components/pages/home/hero-carousel.tsx
@@ -10,16 +10,16 @@ export default function HeroCarousel({strapiSlider}: {strapiSlider: StrapiSlider
     <main className="">
       <Carousel
           slideInterval={10000} // Set the interval to 5000ms (5 seconds), adjust as needed
-        leftControl={(
-          <div>
-            <ChevronLeft className="size-10" strokeWidth={1.2} />
-          </div>
-        )}
-        rightControl={(
-          <div>
-            <ChevronRight className="size-10" strokeWidth={1.2} />
-          </div>
-        )}
+        //leftControl={(
+        //>/  <div>
+        //    <ChevronLeft className="size-10" strokeWidth={1.2} />
+        //  </div>
+        //)}
+        //rightControl={(
+        //  <div>
+        //    <ChevronRight className="size-10" strokeWidth={1.2} />
+        //  </div>
+        //)}
       >
         {strapiSlider.data.map(ai => (
           <CarouselItem key={ai.id} item={ai.attributes} locale="ru" />
