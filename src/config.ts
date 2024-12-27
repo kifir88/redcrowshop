69ce742f21b06cd5ff2968bd90e31d7f61cc49db
thedogrex <vdogrex@gmail.com>
2024-12-27 11:43:50 +0500

    fix types

--- src/config.ts
+++ src/config.ts
@@ -1,11 +1,23 @@
-const config = {
-  "PAGES": {
-    "shipping": "16495",
-    "about": "16497",
-    "oferta": "16492",
-    "confidential": "16499",
-    "payment_error": "16506",
-    "payment_success": "16508"
+interface Config {
+  PAGES: {
+    [key: string]: string;  // Indicates any key is a string that maps to a string
+    shipping: string;
+    about: string;
+    oferta: string;
+    confidential: string;
+    payment_error: string;
+    payment_success: string;
+  };
+}
+
+const config: Config = {
+  PAGES: {
+    shipping: "16495",
+    about: "16497",
+    oferta: "16492",
+    confidential: "16499",
+    payment_error: "16506",
+    payment_success: "16508"
   }
 };
 
