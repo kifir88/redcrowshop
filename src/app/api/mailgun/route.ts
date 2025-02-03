82a306321a87c257c696f8750d7bc86918f6e4ef
thedogrex <vdogrex@gmail.com>
2025-02-03 17:20:25 +0500

    fix multiple emails

--- src/app/api/mailgun/route.ts
+++ src/app/api/mailgun/route.ts
@@ -1,48 +1,55 @@
 import { NextRequest, NextResponse } from "next/server";
-import FormData from 'form-data';
-import Mailgun from 'mailgun.js';
-import {MailgunMessageData} from "mailgun.js/Types/Messages";
+import FormData from "form-data";
+import Mailgun from "mailgun.js";
+import { MailgunMessageData } from "mailgun.js/Types/Messages";
 
 const mailgun = new Mailgun(FormData);
 const mailgunClient = mailgun.client({
   url: "https://api.eu.mailgun.net",
-  username: 'api',
+  username: "api",
   key: process.env.MAILGUN_API_KEY || "9085566ba3ba48dbf8024860b652aba3-d010bdaf-9b12ac21",
 });
+
+// In-memory cache to store the last sent timestamp per email
+const emailCache = new Map<string, number>();
+const EMAIL_COOLDOWN_MS = 3 * 60 * 1000; // 3 minutes
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
 
-    if (
-      !body?.to
-      || !body?.subject
-      || !body?.text
-    ) {
+    if (!body?.to || !body?.subject || !body?.text) {
       throw new Error("В теле запроса не хватает базовых параметров.");
     }
-    // if (
-    //   !(body?.text && body?.html)
-    // ) {
-    //   throw new Error("Параметры html или text обязательны.");
-    // }
+
+    const currentTime = Date.now();
+    const lastSentTime = emailCache.get(body.to);
+
+    if (lastSentTime && currentTime - lastSentTime < EMAIL_COOLDOWN_MS) {
+      return NextResponse.json(
+          { message: "Email was sent less than 3 minutes ago. Ignoring request." },
+          { status: 429 }
+      );
+    }
 
     const mailgunMessageData: MailgunMessageData = {
       from: "REDCROW KZ <mailgun@redcrow.kz>",
       to: [body.to],
       subject: body.subject,
       text: body.text || "",
-    }
+    };
 
     if (body?.html) {
       mailgunMessageData.html = body.html;
     }
 
     const mailgunResponse = await mailgunClient.messages.create(
-      // TODO extract to env variables
-      "redcrow.kz",
-      mailgunMessageData
-    )
+        "redcrow.kz",
+        mailgunMessageData
+    );
+
+    // Update cache with the latest timestamp
+    emailCache.set(body.to, currentTime);
 
     return NextResponse.json(mailgunResponse);
   } catch (error) {
@@ -51,4 +58,4 @@ export async function POST(req: NextRequest) {
   }
 }
 
-export const runtime = 'nodejs'
+export const runtime = "nodejs";
