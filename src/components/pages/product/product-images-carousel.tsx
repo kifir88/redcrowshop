597a7288524ba53b2f4fadd3e58a8347e63a36fe
root@dev.redcrow.kz <root@dev.redcrow.kz>
2026-01-02 21:59:48 +0100

    fix slider images aspect

--- src/components/pages/product/product-images-carousel.tsx
+++ src/components/pages/product/product-images-carousel.tsx
@@ -10,46 +10,46 @@ import Image from "next/image";
 // Right arrow
 function NextArrow({ onClick, disabled }: { onClick?: () => void; disabled: boolean }) {
   return (
-      <button
-          onClick={!disabled ? onClick : undefined}
-          disabled={disabled}
-          style={{
-            position: "absolute",
-            top: "50%",
-            right: "20px",
-            transform: "translateY(-50%)",
-            zIndex: 10,
-            opacity: disabled ? 0.5 : 1,
-            cursor: disabled ? "not-allowed" : "pointer",
-          }}
-          className="bg-white shadow px-3 py-2 text-sm hover:bg-gray-200"
-          aria-label="Next Slide"
-      >
-        ►
-      </button>
+    <button
+      onClick={!disabled ? onClick : undefined}
+      disabled={disabled}
+      style={{
+        position: "absolute",
+        top: "50%",
+        right: "20px",
+        transform: "translateY(-50%)",
+        zIndex: 10,
+        opacity: disabled ? 0.5 : 1,
+        cursor: disabled ? "not-allowed" : "pointer",
+      }}
+      className="bg-white shadow px-3 py-2 text-sm hover:bg-gray-200"
+      aria-label="Next Slide"
+    >
+      ►
+    </button>
   );
 }
 
 // Left arrow
 function PrevArrow({ onClick, disabled }: { onClick?: () => void; disabled: boolean }) {
   return (
-      <button
-          onClick={!disabled ? onClick : undefined}
-          disabled={disabled}
-          style={{
-            position: "absolute",
-            top: "50%",
-            left: "20px",
-            transform: "translateY(-50%)",
-            zIndex: 10,
-            opacity: disabled ? 0.5 : 1,
-            cursor: disabled ? "not-allowed" : "pointer",
-          }}
-          className="bg-white shadow px-3 py-2 text-sm hover:bg-gray-200"
-          aria-label="Previous Slide"
-      >
-        ◄
-      </button>
+    <button
+      onClick={!disabled ? onClick : undefined}
+      disabled={disabled}
+      style={{
+        position: "absolute",
+        top: "50%",
+        left: "20px",
+        transform: "translateY(-50%)",
+        zIndex: 10,
+        opacity: disabled ? 0.5 : 1,
+        cursor: disabled ? "not-allowed" : "pointer",
+      }}
+      className="bg-white shadow px-3 py-2 text-sm hover:bg-gray-200"
+      aria-label="Previous Slide"
+    >
+      ◄
+    </button>
   );
 }
 
@@ -82,25 +82,26 @@ export default function ProductImagesCarousel({ images, productImageIndex }: Pro
   };
 
   return (
-      <div className="mt-8 lg:col-span-7 lg:col-start-1 lg:row-span-3 lg:row-start-1 lg:mt-0">
-        <div className="grid grid-cols-1 lg:gap-8">
-          <div className="aspect-3/4 lg:col-span-2 max-h-[1509px]">
-            <Slider ref={sliderRef} {...settings}>
-              {images.map((img, index) => (
-                  <Image
-                      key={img?.id || index}
-                      src={img?.src}
-                      alt={img?.alt || "placeholder"}
-                      width={696}
-                      height={1002}
-
-
-                  />
-              ))}
-            </Slider>
-          </div>
+    <div className="mt-8 lg:col-span-7 lg:col-start-1 lg:row-span-3 lg:row-start-1 lg:mt-0">
+      <div className="grid grid-cols-1 lg:gap-8">
+        <div className="aspect-3/4 lg:col-span-2 max-h-[1509px]">
+          <Slider ref={sliderRef} {...settings}>
+            {images.map((img, index) => (
+              <div className="relative aspect-[2/3] w-full overflow-hidden">
+                <Image
+                  key={img?.id || index}
+                  src={img?.src}
+                  alt={img?.alt || "placeholder"}
+                  width={696}
+                  height={1002}
+                  className="object-cover"
+                />
+              </div>
+            ))}
+          </Slider>
         </div>
       </div>
+    </div>
   );
 }
 
