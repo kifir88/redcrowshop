8d95f12228aeebbd211401fad2a259fd85904de9
root@dev.redcrow.kz <root@dev.redcrow.kz>
2026-01-07 04:53:05 +0100

    yandex metric

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 
 
 import type { Metadata } from "next";
-import {Roboto} from "next/font/google";
+import { Roboto } from "next/font/google";
 import Providers from "@/app/providers";
 import Navbar from "@/components/layout/navbar";
 import Footer from "@/components/layout/footer";
-import {ThemeModeScript} from "flowbite-react";
-import {SITE_DATA} from "@/metadata/site-data";
+import { ThemeModeScript } from "flowbite-react";
+import { SITE_DATA } from "@/metadata/site-data";
 
 import "./globals.css";
 import FullscreenModal from "@/components/fullscreen_modal";
@@ -22,6 +22,8 @@ export const metadata: Metadata = {
   description: SITE_DATA.desc_home,
 };
 
+const YM_ID = '106161769';
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -30,19 +32,43 @@ export default function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning>
-    <head>
+      <head>
         {/*--<Script src="./cdek.js" strategy="beforeInteractive" />-->*/}
-        <ThemeModeScript/>
-    </head>
-    <body className={roboto.className}>
-    <Providers>
-        <main>
-            <Navbar/>
+        <ThemeModeScript />
+      </head>
+      <body className={roboto.className}>
+        <Providers>
+          <main>
+            <Navbar />
             {children}
             <Footer />
             {/*<FullscreenModal />*/}
           </main>
         </Providers>
+        {/* Яндекс Метрика */}
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+               (function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+        })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=106161769', 'ym');
+
+        ym(106161769, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true});
+
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img
+              src={`https://mc.yandex.ru/watch/{YM_ID}`}
+              style={{ position: 'absolute', left: '-9999px' }}
+              alt=""
+            />
+          </div>
+        </noscript>
+
       </body>
     </html>
   );
